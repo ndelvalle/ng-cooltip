318b0e9ca4cafd82bbab3f020d2d20e258831cf2
ndelvalle <nicolas.delvalle@gmail.com>
2016-01-23 04:08:47 +0100

    Added gulp task to build production javascript and css files

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp  = require('gulp');
+var gutil = require('gulp-util');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass   = require('gulp-sass');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('build-js', function() {
+  return gulp.src('src/js/ng-cooltip.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('ng-cooltip.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-css', function() {
+  return gulp.src('src/styles/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('production', ['build-js', 'build-css']);
