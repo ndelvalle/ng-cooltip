7da83b94904a2b94a24ab09549fac3677bba3ea0
ndelvalle <nicolas.delvalle@gmail.com>
2016-02-14 04:21:04 +0100

    Added travis file and added gulp task test

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ var sass   = require('gulp-sass');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 
+var karma = require('karma').server;
+var isTravis = process.env.TRAVIS || false;
+var pathToKarmaConf = __dirname.replace('/gulp', '');
+
 gulp.task('build-js', function() {
   return gulp.src('src/js/ng-cooltip.js')
     .pipe(sourcemaps.init())
@@ -25,6 +29,13 @@ gulp.task('build-css', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('production', ['build-js', 'build-css']);
+gulp.task('test', function (done) {
+  console.log('isTravis', isTravis);
+  karma.start({
+    configFile: pathToKarmaConf + '/karma.conf.js',
+    singleRun: isTravis
+  }, done);
+});
 
+gulp.task('production', ['build-js', 'build-css']);
 gulp.task('default', ['production']);
